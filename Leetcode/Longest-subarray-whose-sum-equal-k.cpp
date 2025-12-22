c8ef8de1d84fe14608aed9efd221a0197784b44f
X-Cyborg <puneethkshetty6720@gmail.com>
2025-12-22 22:39:05 +0530

    Update Longest-subarray-whose-sum-equal-k.cpp

--- Leetcode/Longest-subarray-whose-sum-equal-k.cpp
+++ Leetcode/Longest-subarray-whose-sum-equal-k.cpp
@@ -1,4 +1,35 @@
+#include<bits/stdc++.h>
+#include<climits>
+#include<numeric>
+#include<algorithm>
+class Solution {
+  public:
+int longestSubarray(vector<int>& arr, int k) {
+    unordered_map<int, int> mp;
+    int prefixsum = 0;
+    int best = 0;
 
+    mp[0] = -1; // handle prefix itself equals k
+
+    for (int i = 0; i < arr.size(); ++i) {
+        prefixsum += arr[i];
+
+        if (mp.find(prefixsum - k) != mp.end()) {
+            int len = i - mp[prefixsum - k];
+            best = max(best, len);
+        }
+
+        if (mp.find(prefixsum) == mp.end()) {
+            mp[prefixsum] = i; // store earliest occurrence
+        }
+    }
+    return best;
+
+    }
+};
+
+#next solution for positive no:
+0(n)
 int longestSubarray(vector<int>& arr, int k) {
     int n = arr.size();
     int left = 0;
